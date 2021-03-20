9ca9a107ec2640e4d9e002d52dd318fd215a5b6f
alexpseletr <alxpslva@gmail.com>
2021-03-20 13:55:38 -0300

    Create App.vue

---
+++ vue2/diretivas/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <H1>minha lista de tarefas</H1>
+    <button @click="() => showlist = !showlist" >ver lista</button>
+    <br>
+    
+    <ul v-if="showlist">
+      <li v-for="task  in tasks" v-bind:key="task"> {{task.name}} </li>  
+    </ul>  
+    <p v-else>tarefas ocultas</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data:() =>({
+    showlist:false,
+    tasks:[
+        {name:"fazer o curso",isdone:false}   
+        ]
+  })
+}
+</script>
+
+<style>
+
+</style>
